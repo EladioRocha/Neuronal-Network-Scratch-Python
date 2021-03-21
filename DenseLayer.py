1dbf5a7315abc64216e5d2b80e870f4f69e1773f
Eladio Rocha Vizcaino <2983321-EladioRocha@users.noreply.gitlab.com>
2021-03-20 19:36:33 -0600

    Accuracy and iteration for search better parameters

--- DenseLayer.py
+++ DenseLayer.py
@@ -1,6 +1,7 @@
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
+from nnfs.datasets import vertical_data
 from ActivationReLU import Activation_ReLU
 from ActivationSoftmax import Activation_Softmax
 from Loss import Loss_CategoricalCrossentropy
@@ -30,6 +31,7 @@ activation_output = activation1.forward(dense_output)
 softmax = Activation_Softmax()
 softmax.forward([[1, 2, 3]])
 
+############################################## INICIO DE SPIRAL DATA #####################################################
 print('============== EJEMPLO USANDO LAS FUNCIONES CON MUESTRAS DE SPIRAL DATA. =============')
 x, y = spiral_data(samples = 100, classes = 3)
 dense1 = Layer_Dense(2, 3)
@@ -48,7 +50,16 @@ print(activation2.output[:5])
 # Calculando la perdida en el ejemplo con la data de spiral.
 loss_function = Loss_CategoricalCrossentropy()
 loss = loss_function.calculate(activation2.output, y)
-print('Loss: ', loss)
+print('Loss spiral: ', loss)
+
+predictions = np.argmax(activation2.output, axis = 1)
+if(len(y.shape) == 2):
+	y = np.argmax(y, axis = 1)
+
+accuracy = np.mean(predictions == y)
+
+print('Accuracy spiral: ', accuracy)
+############################################## FINAL DE SPIRAL DATA #####################################################
 
 ## Usando clase Loss en ejemplo individual.
 softmax_outputs = np.array([[0.7, 0.1, 0.2],
@@ -59,4 +70,78 @@ softmax_outputs = np.array([[0.7, 0.1, 0.2],
 class_targets = np.array([0, 1, 1])
 
 loss = loss_function.calculate(softmax_outputs, class_targets)
-print(loss)
+print('Loss: ', loss)
+
+## Accuracy calculation
+
+## Ejemplo de 3 muestras
+softmax_outputs = np.array([
+	[0.7, 0.2, 0.1],
+	[0.5, 0.1, 0.4],
+	[0.02, 0.9, 0.08]
+])
+
+# Etiqueta de las 3 muestras
+class_targets = np.array([0, 1, 1])
+# Obtiene los valores máximos de cada posicion -> [0 0 1]
+predictions = np.argmax(softmax_outputs, axis = 1)
+if len(class_targets.shape) == 2:
+  class_targets = np.argmax(class_targets, axis=1)
+
+accuracy = np.mean(predictions == class_targets)
+print('Accuracy: ', accuracy)
+
+########################## EJEMPLOS USANDO DATA VERTICAL ######################################
+X, y = vertical_data(samples = 100, classes = 3)
+dense1 = Layer_Dense(2, 3) ## primera capa 2 entradas
+activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3, 3) ## 3 entradas, 3 salidas
+activation2 = Activation_Softmax()
+
+## Función de perdida
+loss_function = Loss_CategoricalCrossentropy()
+
+lowest_loss = 9999999  # Valor inicial
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+
+for iteration in range(10000):
+	# Actualizar los pesos con algunos valores pequeños
+	dense1.weights += 0.05 * np.random.randn(2, 3)
+	dense1.biases += 0.05 * np.random.randn(1, 3)
+	dense2.weights += 0.05 * np.random.randn(3, 3)
+	dense2.biases += 0.05 * np.random.randn(1, 3)
+
+	## Pasa a las siguientes capas
+	dense1.forward(X)
+	activation1.forward(dense1.output)
+	dense2.forward(activation1.output)
+	activation2.forward(dense2.output)
+
+	# calculo del error con la salidad de la segunda capa de activacion y pasandole las labels
+	loss = loss_function.calculate(activation2.output, y)
+
+
+	# Calculo de la precisión con los datos de la capa de salida
+	predictions = np.argmax(activation2.output, axis=1)
+	accuracy = np.mean(predictions==y)
+
+	# Si la perdida es menor, guarda los valores y se imprimen en pantalla
+	if loss < lowest_loss:
+			print('New set of weights found, iteration:', iteration,
+						'loss:', loss, 'acc:', accuracy)
+			best_dense1_weights = dense1.weights.copy()
+			best_dense1_biases = dense1.biases.copy()
+			best_dense2_weights = dense2.weights.copy()
+			best_dense2_biases = dense2.biases.copy()
+			lowest_loss = loss
+	# Revierte los pesos y bias pues los valores no fueron mejores
+	else:
+			dense1.weights = best_dense1_weights.copy()
+			dense1.biases = best_dense1_biases.copy()
+			dense2.weights = best_dense2_weights.copy()
+			dense2.biases = best_dense2_biases.copy()
+
+## 149
--- nnfs-practice.py
+++ nnfs-practice.py
@@ -1,8 +1,14 @@
 import numpy as np
-from nnfs.datasets import spiral_data
 import matplotlib.pyplot as plt
+from nnfs.datasets import spiral_data
+from nnfs.datasets import vertical_data
+
 
 x, y = spiral_data(samples = 100, classes = 3)
 print(len(y))
 plt.scatter(x[:, 0], x[:, 1], c = y, cmap = 'brg')
+plt.show()
+
+X, y = vertical_data(samples = 100, classes = 3)
+plt.scatter(X[:, 0], X[:, 1], c = y, s = 40, cmap = 'brg')
 plt.show()
